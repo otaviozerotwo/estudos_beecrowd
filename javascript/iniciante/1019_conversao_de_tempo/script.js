59c828797acf7667f29ef478d93f0d2b7c2ee4c5
Otavio <otavio.zerotwo@gmail.com>
2026-07-17 20:15:30 -0300

    feat: add atividades 1019, 1020, 1021 e 1035

---
+++ javascript/iniciante/1019_conversao_de_tempo/script.js
@@ -0,0 +1,21 @@
+/* Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+
+* Entrada
+O arquivo de entrada contém um valor inteiro N.
+
+* Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido. */
+
+const prompt = require('prompt-sync')();
+
+const tempoEmSegundos = Number(prompt());
+
+const horas = tempoEmSegundos / 3600;
+let restoDaDivisao = tempoEmSegundos % 3600;
+
+const minutos = restoDaDivisao / 60;
+restoDaDivisao = restoDaDivisao % 60;
+
+const segundos = restoDaDivisao;
+
+console.log(`${Math.trunc(horas)}:${Math.trunc(minutos)}:${Math.trunc(segundos)}`);
---
+++ javascript/iniciante/1020_idade_em_dias/script.js
@@ -0,0 +1,25 @@
+/* Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
+* Entrada
+O arquivo de entrada contém um valor inteiro.
+
+* Saída
+Imprima a saída conforme exemplo fornecido. */
+
+const prompt = require('prompt-sync')();
+
+const idadeEmDias = Number(prompt());
+
+const anos = Math.trunc(idadeEmDias / 365);
+let restoDaDivisao = idadeEmDias % 365;
+
+const meses = Math.trunc(restoDaDivisao / 30);
+restoDaDivisao = restoDaDivisao % 30;
+
+const dias = Math.trunc(restoDaDivisao);
+
+console.log(`${anos} ano(s)`);
+console.log(`${meses} mes(es)`);
+console.log(`${dias} dia(s)`);
